668a1f1570542d46eb75aec800fb335a7e7eaa2a
edichu <edi.chua@ymail.com>
2019-08-01 23:02:42 +0800

    Update Aug 1

--- api/SeatSvc.js
+++ api/SeatSvc.js
@@ -6,10 +6,25 @@ const loadEndpoint = (app) => {
         console.log('URL: ' + req.url);
         Seat
             .query()
-            // .findById(['S1', 'ADS'])
             .eager('reservations')
             .then((result) => { res.send(result) });
     });
+
+    app.get('/seats/[A-Z0-9]+/[A-Z0-9]+*', (req, res) => {
+        console.log('URL: ' + req.url);
+        const tokens = req.url.split("/");
+        const location = tokens[2];
+        const seat = tokens[3];
+
+        Seat
+            .query()
+            .findById([seat, location])
+            .eager('reservations')
+            .then((result) => { console.log(result); return result })
+            .then((result) => { res.send((result)) });
+    });
 }
+
+
 
 module.exports = loadEndpoint;
