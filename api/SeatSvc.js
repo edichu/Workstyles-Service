f0422112ffe981717136c796a0d2dd681309f77d
edichu <edi.chua@ymail.com>
2019-08-03 05:25:10 +0800

    Aug 3 update

--- api/SeatSvc.js
+++ api/SeatSvc.js
@@ -10,21 +10,45 @@ const loadEndpoint = (app) => {
             .then((result) => { res.send(result) });
     });
 
-    app.get('/seats/[A-Z0-9]+/[A-Z0-9]+*', (req, res) => {
+    app.get('/seats/[A-Z0-9]+/[A-Z0-9]+', (req, res) => {
         console.log('URL: ' + req.url);
         const tokens = req.url.split("/");
         const location = tokens[2];
         const seat = tokens[3];
+        const date = tokens[4];
 
         Seat
             .query()
             .findById([seat, location])
             .eager('reservations')
-            .then((result) => { console.log(result); return result })
+            .then((result) => { if (result) { return result } else { return {} } })
             .then((result) => { res.send((result)) });
+    });
+
+    app.get('/seats/[A-Z0-9]+/[A-Z0-9]+/*', (req, res) => {
+        const tokens = req.url.split("/");
+        const location = tokens[2];
+        const seat = tokens[3];
+        const date = tokens[4];
+
+        console.log('URL: ' + req.url + '. Date: ' + date);
+
+        Seat
+            .query()
+            .findById([seat, location])
+            .eager('reservations')
+            .then((result) => { res.send((filterByDate(date, result))) });
     });
 }
 
+const filterByDate = (date, result) => {
+    if (result) {
+        result.reservations = result.reservations.filter(reservation => reservation.date === date);
 
+        return result;
+    } else {
+        return {};
+    }
+}
 
 module.exports = loadEndpoint;
